de7216113cf1f53f7f561397d288f7ec41de81f2
annesilv4 <carolaynebarbozasilva@gmail.com>
2024-12-04 18:06:03 -0300

    adding button settings (previous and next)

--- script.js
+++ script.js
@@ -14,9 +14,13 @@ nextButton.onclick = () => {
     itemOld.classList.remove('active');
 
     active = active + 1 > lastPosition ? 0 : active + 1
-    items[active];
+    items[active].classList.add('active');
 }
 
 prevButton.onclick = () => {
+    let itemOld = container.querySelector('.list .item.active');
+    itemOld.classList.remove('active');
 
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    items[active].classList.add('active');
 }
